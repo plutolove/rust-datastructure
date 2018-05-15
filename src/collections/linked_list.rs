2e6b7c58fa51f04334e4b2623782c1c0da5de152
mengshangqi <mengshangqis@gmail.com>
2018-05-15 13:49:21 +0800

    add iter and test

--- src/collections/linked_list.rs
+++ src/collections/linked_list.rs
@@ -14,15 +14,31 @@ pub struct LinkedList<T> {
     marker: PhantomData<Box<Node<T>>>,
 }
 
+pub struct Iter<'a, T: 'a> {
+    head: Option<NonNull<Node<T>>>,
+    tail: Option<NonNull<Node<T>>>,
+    len: usize,
+    marker: PhantomData<&'a T>,
+}
+
 impl<T> Node<T> {
-    fn new(elem: T)-> Self {
-        Node{next: None, prev: None, elem: elem}
+    fn new(elem: T) -> Self {
+        Node {
+            next: None,
+            prev: None,
+            elem: elem,
+        }
     }
-    fn into_elem(self: Box<Self>)-> T {
+    fn into_elem(self: Box<Self>) -> T {
         self.elem
     }
 }
 
+impl<'a, T> Clone for Iter<'a, T> {
+    fn clone(&self) -> Self {
+        Iter { ..*self }
+    }
+}
 
 impl<T> LinkedList<T> {
     #[inline]
@@ -42,7 +58,7 @@ impl<T> LinkedList<T> {
     }
 
     #[inline]
-    fn pop_front_node(&mut self)-> Option<Box<Node<T>>> {
+    fn pop_front_node(&mut self) -> Option<Box<Node<T>>> {
         self.head.map(|node| unsafe {
             let node = Box::from_raw(node.as_ptr());
             self.head = node.next;
@@ -73,7 +89,7 @@ impl<T> LinkedList<T> {
     }
 
     #[inline]
-    fn pop_back_node(&mut self)-> Option<Box<Node<T>>> {
+    fn pop_back_node(&mut self) -> Option<Box<Node<T>>> {
         self.tail.map(|node| unsafe {
             let node = Box::from_raw(node.as_ptr());
             self.tail = node.prev;
@@ -82,24 +98,46 @@ impl<T> LinkedList<T> {
                 None => self.head = None,
                 Some(mut tail) => tail.as_mut().next = None,
             }
+            self.len -= 1;
             node
         })
     }
 }
 
+unsafe impl<#[may_dangle] T> Drop for LinkedList<T> {
+    fn drop(&mut self) {
+        while let Some(_) = self.pop_front_node() {}
+    }
+}
+
 impl<T> LinkedList<T> {
     #[inline]
-    pub fn new()-> Self {
-        LinkedList{head: None, tail: None, len: 0, marker: PhantomData}
+    pub fn iter(&self) -> Iter<T> {
+        Iter {
+            head: self.head,
+            tail: self.tail,
+            len: self.len,
+            marker: PhantomData,
+        }
     }
 
     #[inline]
-    pub fn is_empty(&self)-> bool {
+    pub fn new() -> Self {
+        LinkedList {
+            head: None,
+            tail: None,
+            len: 0,
+            marker: PhantomData,
+        }
+    }
+
+    #[inline]
+    pub fn is_empty(&self) -> bool {
         self.head.is_none()
     }
 
     #[inline]
-    pub fn len(&self)-> usize {
+    pub fn len(&self) -> usize {
         self.len
     }
 
@@ -109,38 +147,30 @@ impl<T> LinkedList<T> {
     }
 
     #[inline]
-    pub fn front(&self)-> Option<&T> {
-        unsafe {
-            self.head.as_ref().map(|node| &node.as_ref().elem)
-        }
+    pub fn front(&self) -> Option<&T> {
+        unsafe { self.head.as_ref().map(|node| &node.as_ref().elem) }
     }
 
     #[inline]
-    pub fn front_mut(&mut self)-> Option<&mut T> {
-        unsafe {
-            self.head.as_mut().map(|node| &mut node.as_mut().elem)
-        }
+    pub fn front_mut(&mut self) -> Option<&mut T> {
+        unsafe { self.head.as_mut().map(|node| &mut node.as_mut().elem) }
     }
 
     #[inline]
-    pub fn back(&self)-> Option<&T> {
-        unsafe {
-            self.tail.as_ref().map(|node| &node.as_ref().elem)
-        }
+    pub fn back(&self) -> Option<&T> {
+        unsafe { self.tail.as_ref().map(|node| &node.as_ref().elem) }
     }
 
     #[inline]
-    pub fn back_mut(&mut self)-> Option<&mut T> {
-        unsafe {
-            self.tail.as_mut().map(|node| &mut node.as_mut().elem)
-        }
+    pub fn back_mut(&mut self) -> Option<&mut T> {
+        unsafe { self.tail.as_mut().map(|node| &mut node.as_mut().elem) }
     }
 
     pub fn push_front(&mut self, elt: T) {
         self.push_front_node(Box::new(Node::new(elt)));
     }
 
-    pub fn pop_front(&mut self)-> Option<T> {
+    pub fn pop_front(&mut self) -> Option<T> {
         self.pop_front_node().map(Node::into_elem)
     }
 
@@ -148,7 +178,65 @@ impl<T> LinkedList<T> {
         self.push_back_node(Box::new(Node::new(elt)));
     }
 
-    pub fn pop_back(&mut self)-> Option<T> {
+    pub fn pop_back(&mut self) -> Option<T> {
         self.pop_back_node().map(Node::into_elem)
     }
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+    #[inline]
+    fn next(&mut self) -> Option<&'a T> {
+        if self.len == 0 {
+            None
+        } else {
+            self.head.map(|node| unsafe {
+                let node = &*node.as_ptr();
+                self.len -= 1;
+                self.head = node.next;
+                &node.elem
+            })
+        }
+    }
+
+    #[inline]
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        (self.len, Some(self.len))
+    }
+}
+
+#[test]
+fn test_push_pop_front() {
+    let mut list = LinkedList::new();
+    list.push_front(1);
+    list.push_front(2);
+    list.push_front(3);
+    list.push_front(4);
+    assert_eq!(list.len, 4);
+    list.pop_front();
+    assert_eq!(list.len, 3);
+}
+#[test]
+fn test_push_pop_back() {
+    let mut list = LinkedList::new();
+    list.push_back(2);
+    list.push_back(54);
+    list.push_back(3556);
+    list.push_back(45656);
+    assert_eq!(list.len, 4);
+    list.pop_back();
+    assert_eq!(list.len, 3);
+}
+#[test]
+fn test_iter() {
+    let mut list: LinkedList<u32> = LinkedList::new();
+
+    list.push_back(0);
+    list.push_back(1);
+    list.push_back(2);
+    let mut iter = list.iter();
+    assert_eq!(iter.next(), Some(&0));
+    assert_eq!(iter.next(), Some(&1));
+    assert_eq!(iter.next(), Some(&2));
+    assert_eq!(iter.next(), None);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
 #![feature(box_into_raw_non_null)]
+#![feature(dropck_eyepatch)]
 mod collections;
 use collections::linked_list::LinkedList;
 
 fn main() {
     let mut list = LinkedList::new();
     list.push_back(1);
-    list.push_back(1);
-    list.push_back(1);
-    list.push_back(1);
+    list.push_back(2);
+    list.push_back(3);
+    list.push_back(4);
+
+    for item in list.iter() {
+        println!("{}", item);
+    }
 }
